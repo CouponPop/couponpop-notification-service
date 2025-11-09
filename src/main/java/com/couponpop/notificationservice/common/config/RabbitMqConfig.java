6c3ec9e5b4752cb023999eaf32184c10d69dbffb
younghunkimm <hunifive@gmail.com>
2025-11-09 21:37:00 +0900

    bug: NonRetryableFcmException 예외가 발생하더라도 재시도하는 버그 수정

--- src/main/java/com/couponpop/notificationservice/common/config/RabbitMqConfig.java
+++ src/main/java/com/couponpop/notificationservice/common/config/RabbitMqConfig.java
@@ -1,6 +1,7 @@
 package com.couponpop.notificationservice.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
@@ -11,12 +12,16 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.interceptor.RetryOperationsInterceptor;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableRabbit
 @Configuration
 public class RabbitMqConfig {
 
-    private static final int MAX_ATTEMPTS = 5;
+    private static final int MAX_ATTEMPTS = 5; // 최대 재시도 횟수
     private static final int BACKOFF_INITIAL_INTERVAL = 1_000; // 1 second
     private static final double BACKOFF_MULTIPLIER = 2.0;
     private static final int BACKOFF_MAX_INTERVAL = 30_000; // 30 seconds
@@ -44,9 +49,20 @@ public class RabbitMqConfig {
             MessageConverter rabbitListenerMessageConverter
     ) {
 
+        // AmqpRejectAndDontRequeueException은 즉시 DLQ로 보내도록 재시도 대상에서 제외
+        Map<Class<? extends Throwable>, Boolean> retryableExceptions = new HashMap<>();
+        retryableExceptions.put(AmqpRejectAndDontRequeueException.class, false);
+
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy(
+                MAX_ATTEMPTS, // 최대 재시도 횟수
+                retryableExceptions, // 재시도 대상 예외 설정
+                true // 나머지 예외는 재시도 대상
+        );
+
         // 재시도 인터셉터 설정
         RetryOperationsInterceptor retryAdvice = RetryInterceptorBuilder.stateless()
-                .maxAttempts(MAX_ATTEMPTS)
+                // 재시도 정책 설정
+                .retryPolicy(retryPolicy)
                 // 지수 백오프 기반 재시도 설정 (1초, 2초, 4초, 8초, ...)
                 .backOffOptions(BACKOFF_INITIAL_INTERVAL, BACKOFF_MULTIPLIER, BACKOFF_MAX_INTERVAL)
                 // RetryInterceptor가 최대 재시도 횟수를 모두 소진했을 때 호출되는 복구 전략 (재큐잉하지 않고 바로 DLX로 보냄)
